9ae60d777d8b9f0f9b61644ba20712e5d7747f3a
Diego Botero <diego.botero@correounivalle.edu.co>
2018-08-20 21:48:27 -0500

    Registra Profile en Django admin. Modelo: Post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class Post(models.Model):
+
+	title = models.CharField(max_length=100)
+	photo = models.ImageField(upload_to='posts/photos')
+	
+	created = models.DateTimeField(auto_now_add=True)
+	last_modified = models.DateTimeField(auto_now=True)
+	
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+	
+	def __str__(self):
+	
+		return '{} by @{}'.format(self.title, self.user.username)
