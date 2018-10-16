30d90183e968cc7b0fe0e0bd7795500480f89c8d
Diego Botero <diego.botero@correounivalle.edu.co>
2018-10-15 19:25:41 -0500

    Pagina y funcionalidad de Login. Se añade atributo name a urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+
+# View login 
+def login_view(request):
+
+	if request.method == 'POST':
+		
+		username = request.POST['username']
+		password = request.POST['password']
+		#print("POST ", username, ":", password)
+		user = authenticate(request, username=username, password=password)
+		
+		if user:
+			login(request, user)
+			return redirect('feed')
+		else:
+			return render(request, 'users/login.html',
+				{'error': 'Invalid username or password'}
+				)
+	return render(request, 'users/login.html')
